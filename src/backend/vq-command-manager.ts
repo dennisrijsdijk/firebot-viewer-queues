f34ee715cbcb9647f0d0377c47ee197da4e885c3
dennisrijsdijk <git@dennis.gg>
2025-09-12 10:32:23 +0200

    command/subcommand verbiage

--- src/backend/vq-command-manager.ts
+++ src/backend/vq-command-manager.ts
@@ -180,7 +180,7 @@ export class VqCommandManager {
                     baseCommandNotJoinedTemplate: {
                         type: "string",
                         title: "Main Command Template (Not Joined)",
-                        description: "How the !queue command should respond when the user is not in the queue, and the queue is not random.",
+                        description: "How the queue command should respond when the user is not in the queue, and the queue is not random.",
                         tip: "Variables: {username}, {queueName}, {queueLength}, {queueStatus}",
                         default: "The queue is currently {queueStatus} and there are {queueLength} people in the queue.",
                         useTextArea: true,
@@ -188,7 +188,7 @@ export class VqCommandManager {
                     baseCommandJoinedRandomTemplate: {
                         type: "string",
                         title: "Main Command Template (Joined, Random Queue)",
-                        description: "How the !queue command should respond when the user is in the queue, and the queue is random.",
+                        description: "How the queue command should respond when the user is in the queue, and the queue is random.",
                         tip: "Variables: {username}, {queueName}, {queueLength}, {queueStatus}",
                         default: "The queue is currently {queueStatus} and there are {queueLength} people in the queue, including you.",
                         useTextArea: true,
@@ -196,7 +196,7 @@ export class VqCommandManager {
                     baseCommandJoinedTemplate: {
                         type: "string",
                         title: "Main Command Template (Joined, Non-Random Queue)",
-                        description: "How the !queue command should respond when the user is in the queue, and the queue is not random.",
+                        description: "How the queue command should respond when the user is in the queue, and the queue is not random.",
                         tip: "Variables: {username}, {queueName}, {queuePosition}, {queuePeopleAhead}, {queueLength}, {queueStatus}",
                         default: "The queue is currently {queueStatus} and you are #{queuePosition}/{queueLength} in the queue.",
                         useTextArea: true,
@@ -205,7 +205,7 @@ export class VqCommandManager {
                     joinCommandAlreadyJoinedTemplate: {
                         type: "string",
                         title: "Join Command Template (Already Joined)",
-                        description: "How the !join command should respond when the user is already in the queue.",
+                        description: "How the join subcommand should respond when the user is already in the queue.",
                         tip: "Variables: {username}, {queueName}, {queuePosition}, {queuePeopleAhead}, {queueLength}",
                         default: "You are already in the queue, {username}! You are currently #{queuePosition}/{queueLength}.",
                         useTextArea: true,
@@ -213,7 +213,7 @@ export class VqCommandManager {
                     joinCommandAlreadyJoinedRandomTemplate: {
                         type: "string",
                         title: "Join Command Template (Already Joined, Random Queue)",
-                        description: "How the !join command should respond when the user is already in the queue, and the queue is random.",
+                        description: "How the join subcommand should respond when the user is already in the queue, and the queue is random.",
                         tip: "Variables: {username}, {queueName}, {queueLength}",
                         default: "You are already in the queue, {username}!",
                         useTextArea: true
@@ -221,7 +221,7 @@ export class VqCommandManager {
                     joinCommandClosedTemplate: {
                         type: "string",
                         title: "Join Command Template (Queue Closed)",
-                        description: "How the !join command should respond when the queue is closed.",
+                        description: "How the join subcommand should respond when the queue is closed.",
                         tip: "Variables: {username}, {queueName}",
                         default: "Sorry {username}, the queue is currently closed.",
                         useTextArea: true,
@@ -229,7 +229,7 @@ export class VqCommandManager {
                     joinCommandJoinedTemplate: {
                         type: "string",
                         title: "Join Command Template (Joined, Non-Random Queue)",
-                        description: "How the !join command should respond when the user successfully joins the queue, and the queue is not random.",
+                        description: "How the join subcommand should respond when the user successfully joins the queue, and the queue is not random.",
                         tip: "Variables: {username}, {queueName}, {queuePosition}, {queuePeopleAhead}, {queueLength}",
                         default: "You have joined the queue, {username}! You are currently #{queuePosition}/{queueLength}.",
                         useTextArea: true,
@@ -237,7 +237,7 @@ export class VqCommandManager {
                     joinCommandJoinedRandomTemplate: {
                         type: "string",
                         title: "Join Command Template (Joined, Random Queue)",
-                        description: "How the !join command should respond when the user successfully joins the queue, and the queue is random.",
+                        description: "How the join subcommand should respond when the user successfully joins the queue, and the queue is random.",
                         tip: "Variables: {username}, {queueName}, {queueLength}",
                         default: "You have joined the queue, {username}! There are currently {queueLength} people in the queue.",
                         useTextArea: true,
@@ -246,7 +246,7 @@ export class VqCommandManager {
                     positionCommandNotJoinedTemplate: {
                         type: "string",
                         title: "Position Command Template (Not Joined)",
-                        description: "How the !position command should respond when the user is not in the queue.",
+                        description: "How the position subcommand should respond when the user is not in the queue.",
                         tip: "Variables: {username}, {queueName}, {queueLength}, {queueStatus}",
                         default: "You are not currently in the queue, {username}.",
                         useTextArea: true,
@@ -254,7 +254,7 @@ export class VqCommandManager {
                     positionCommandPositionTemplate: {
                         type: "string",
                         title: "Position Command Template (Position)",
-                        description: "How the !position command should respond when reporting the user's position in the queue.",
+                        description: "How the position subcommand should respond when reporting the user's position in the queue.",
                         tip: "Variables: {username}, {queueName}, {queuePosition}, {queuePeopleAhead}, {queueLength}, {queueStatus}",
                         default: "Your position in the queue is #{queuePosition}/{queueLength}, {username}.",
                         useTextArea: true,
@@ -262,7 +262,7 @@ export class VqCommandManager {
                     positionCommandIsRandomTemplate: {
                         type: "string",
                         title: "Position Command Template (Random Queue)",
-                        description: "How the !position command should respond when the queue is random.",
+                        description: "How the position subcommand should respond when the queue is random.",
                         tip: "Variables: {username}, {queueName}, {queueLength}, {queueStatus}",
                         default: "You are currently in the queue, {username}.",
                         useTextArea: true,
@@ -271,7 +271,7 @@ export class VqCommandManager {
                     leaveCommandNotJoinedTemplate: {
                         type: "string",
                         title: "Leave Command Template (Not Joined)",
-                        description: "How the !leave command should respond when the user is not in the queue.",
+                        description: "How the leave subcommand should respond when the user is not in the queue.",
                         tip: "Variables: {username}, {queueName}",
                         default: "You are not currently in the queue, {username}.",
                         useTextArea: true,
@@ -279,7 +279,7 @@ export class VqCommandManager {
                     leaveCommandLeftTemplate: {
                         type: "string",
                         title: "Leave Command Template (Left)",
-                        description: "How the !leave command should respond when the user successfully leaves the queue.",
+                        description: "How the leave subcommand should respond when the user successfully leaves the queue.",
                         tip: "Variables: {username}, {queueName}, {queueLength}",
                         default: "You have successfully left the queue, {username}.",
                         useTextArea: true,
@@ -288,7 +288,7 @@ export class VqCommandManager {
                     clearCommandClearedTemplate: {
                         type: "string",
                         title: "Clear Command Template",
-                        description: "How the !clear command should respond when the queue is cleared.",
+                        description: "How the clear subcommand should respond when the queue is cleared.",
                         tip: "Variables: {queueName}",
                         default: "The queue has been cleared.",
                         useTextArea: true,
@@ -297,7 +297,7 @@ export class VqCommandManager {
                     pickCommandPickedTemplate: {
                         type: "string",
                         title: "Pick Command Template",
-                        description: "How the !pick command should respond when a viewer is picked from the queue.",
+                        description: "How the pick subcommand should respond when a viewer is picked from the queue.",
                         tip: "Variables: {users}, {queueName}, {queueLength}",
                         default: "{users}, you're up next! There are {queueLength} people remaining in the queue.",
                         useTextArea: true
@@ -305,7 +305,7 @@ export class VqCommandManager {
                     pickCommandNoViewersTemplate: {
                         type: "string",
                         title: "Pick Command Template (No Viewers)",
-                        description: "How the !pick command should respond when there are no viewers in the queue.",
+                        description: "How the pick subcommand should respond when there are no viewers in the queue.",
                         tip: "Variables: {queueName}",
                         default: "There is nobody in the queue to pick from.",
                         useTextArea: true
@@ -319,14 +319,14 @@ export class VqCommandManager {
                     pickCommandAutoSplitCount: {
                         type: "number",
                         title: "Pick Command - Auto Split Count",
-                        description: "If picking multiple viewers, how many viewers to attempt to put in each message.",
+                        description: "If picking multiple viewers, how many viewers to put in each message.",
                         default: 3,
                     },
 
                     openCommandOpenedTemplate: {
                         type: "string",
                         title: "Open Command Template (Opened)",
-                        description: "How the !open command should respond when the queue is opened.",
+                        description: "How the open subcommand should respond when the queue is opened.",
                         tip: "Variables: {queueName}",
                         default: "The queue is now open!",
                         useTextArea: true,
@@ -334,7 +334,7 @@ export class VqCommandManager {
                     openCommandAlreadyOpenTemplate: {
                         type: "string",
                         title: "Open Command Template (Already Open)",
-                        description: "How the !open command should respond when the queue is already open.",
+                        description: "How the open subcommand should respond when the queue is already open.",
                         tip: "Variables: {queueName}",
                         default: "The queue is already open.",
                         useTextArea: true,
@@ -343,7 +343,7 @@ export class VqCommandManager {
                     closeCommandClosedTemplate: {
                         type: "string",
                         title: "Close Command Template (Closed)",
-                        description: "How the !close command should respond when the queue is closed.",
+                        description: "How the close subcommand should respond when the queue is closed.",
                         tip: "Variables: {queueName}",
                         default: "The queue is now closed!",
                         useTextArea: true,
@@ -351,7 +351,7 @@ export class VqCommandManager {
                     closeCommandAlreadyClosedTemplate: {
                         type: "string",
                         title: "Close Command Template (Already Closed)",
-                        description: "How the !close command should respond when the queue is already closed.",
+                        description: "How the close subcommand should respond when the queue is already closed.",
                         tip: "Variables: {queueName}",
                         default: "The queue is already closed.",
                         useTextArea: true,
